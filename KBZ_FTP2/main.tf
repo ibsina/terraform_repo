aa16bcaa25fa87e61d65a144d5eb3d547b526bc5
ibsina <ibsina@gmail.com>
2022-10-30 09:08:28 +0800

    aa

---
+++ KBZ_FTP2/main.tf
@@ -0,0 +1,119 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+  profile = var.profile_name
+}
+
+# Create a VPC
+resource "aws_vpc" "app_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "kbz_ftp2-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = {
+    Name = "kbz_ftp2_vpc_igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = "ap-southeast-1a"
+
+  tags = {
+    Name = "kbz_ftp2-public-subnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "kbz_ftp2-public_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Attachment to TGW
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-mgmt" {
+  subnet_ids                                      = [aws_subnet.public_subnet.id]
+  transit_gateway_id                              = tgw-017d8f02a13008cc3
+  vpc_id                                          = aws_vpc.app_vpc.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name     = "tgw-att-kbz-ftp2"
+  }
+
+resource "aws_instance" "kbz-ftp2" {
+  ami           = "ami-0a46ef2b5534a90d6" 
+  instance_type = var.instance_type
+  key_name = var.instance_key
+  subnet_id              = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+
+  tags = {
+    Name = "ftp-kbz2"
+  }
+
+  volume_tags = {
+    Name = "kbz-ftp2"
+  } 
+}
+resource "aws_instance" "linux_pcap2" {
+  ami           = "ami-094bbd9e922dc515d" 
+  instance_type = var.instance_type
+  key_name = var.instance_key
+  subnet_id              = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+ 
+  # data disk
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_size           = "50"
+    volume_type           = "gp2"
+    encrypted             = true
+    delete_on_termination = true
+  }
+  tags = {
+    Name = "linux-pcap2"
+  }
+
+  volume_tags = {
+    Name = "linux-pcap2"
+  } 
+}
+
