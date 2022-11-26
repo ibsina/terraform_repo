9435b864fb669dc5fcb0bc7b171cbea79df42f34
ibsina <ibsina@gmail.com>
2022-11-26 07:44:08 +0700

    aa

---
+++ Marketing_web/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+default = "ap-southeast-1"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "profile_name" {
+default = "default"
+}
+variable "instance_key" {
+default = "fgt_sg"
+}
+variable "vpc_cidr" {
+default = "10.205.10.0/24"
+}
+variable "public_subnet_cidr" {
+default = "10.205.10.0/28"
+}
