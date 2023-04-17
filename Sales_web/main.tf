e5b1908cd365c03c12036932f50cd94853bbe41f
EC2 Default User <ec2-user@ip-10-124-0-235.ap-southeast-1.compute.internal>
2023-04-17 02:54:25 +0000

    aa

--- Sales_web/main.tf
+++ Sales_web/main.tf
@@ -18,17 +18,19 @@ resource "aws_vpc" "app_vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "kbz_ftp1-vpc"
+    Name = "sales-vpc"
   }
 }
 
+/*
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.app_vpc.id
 
   tags = {
-    Name = "kbz_ftp1_vpc_igw"
+    Name = "sales_vpc_igw"
   }
 }
+*/
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.app_vpc.id
@@ -37,20 +39,21 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = "ap-southeast-1a"
 
   tags = {
-    Name = "kbz_ftp1-public-subnet"
+    Name = "sales-public-subnet"
   }
 }
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.app_vpc.id
+  transit_gateway_id = "tgw-017d8f02a13008cc3"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = transit_gateway_id
   }
 
   tags = {
-    Name = "kbz_ftp1-public_rt"
+    Name = "sales-public_rt"
   }
 }
 
@@ -60,20 +63,20 @@ resource "aws_route_table_association" "public_rt_asso" {
 }
 
 # Attachment to TGW
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-kbz-ftp1" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att-sales" {
   subnet_ids                                      = [aws_subnet.public_subnet.id]
   transit_gateway_id                              = "tgw-017d8f02a13008cc3"
   vpc_id                                          = aws_vpc.app_vpc.id
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
   tags = {
-    Name     = "tgw-att-kbz-ftp1"
+    Name     = "tgw-att-sales"
   }
 }
 
-resource "aws_instance" "kbz-ftp1" {
+resource "aws_instance" "sales-vm" {
   ami           = "ami-0a46ef2b5534a90d6" 
-  instance_type = "m5.large"
+  instance_type = "t2.micro"
   key_name = var.instance_key
   subnet_id              = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.sg.id]
@@ -85,44 +88,9 @@ resource "aws_instance" "kbz-ftp1" {
   sudo apt install apache2 -y
   echo "*** Completed Installing apache2"
   EOF
-
-  ebs_block_device {
-    device_name           = "/dev/sda1"
-    volume_size           = "100"
-    volume_type           = "gp2"
-    encrypted             = true
-    delete_on_termination = true
-  }
-
+  
   tags = {
-    Name = "kbz-ftp1"
+    Name = "seahk-is-sales-vm"
   }
-
-  volume_tags = {
-    Name = "kbz-ftp1"
-  } 
-}
-resource "aws_instance" "linux_pcap1" {
-  ami           = "ami-094bbd9e922dc515d" 
-  instance_type = var.instance_type
-  key_name = var.instance_key
-  subnet_id              = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.sg.id]
- 
-  # data disk
-  ebs_block_device {
-    device_name           = "/dev/xvda"
-    volume_size           = "100"
-    volume_type           = "gp2"
-    encrypted             = true
-    delete_on_termination = true
-  }
-  tags = {
-    Name = "linux_pcap1"
-  }
-
-  volume_tags = {
-    Name = "linux_pcap1"
-  } 
 }
 
