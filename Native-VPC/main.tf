be0c9424a3d28c85b08efd94c4734e0cdf2c9526
ibsina <ibsina@gmail.com>
2024-05-29 12:10:08 +0800

    ss

--- Native-VPC/main.tf
+++ Native-VPC/main.tf
@@ -75,7 +75,7 @@ resource "aws_instance" "native_linux" {
   EOF
   ebs_block_device {
     device_name           = "/dev/sda1"
-    volume_size           = "50"
+    volume_size           = "8"
     volume_type           = "gp2"
     encrypted             = true
     delete_on_termination = true
