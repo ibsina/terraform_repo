b98de9c7dc452ff0c2f0f431af7395b71da8e2ef
ibsina <ibsina@gmail.com>
2024-05-30 14:37:12 +0800

    aa

--- Sales_web/sg.tf
+++ Sales_web/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.app_vpc.id
+  vpc_id      = "vpc-0d0d91192dd39889e"
 
   ingress {
     description      = "SSH from VPC"
@@ -15,10 +15,18 @@ resource "aws_security_group" "sg" {
   ingress {
     description      = "all from local"
     from_port        = 0
-    to_port          = 0
+    to_port          = 65535
     protocol         = "tcp"
     cidr_blocks      = ["10.0.0.0/8"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "all from SGIC"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["172.19.0.0/16"]
   }
 
   egress {
